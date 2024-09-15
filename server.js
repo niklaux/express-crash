00da43764ea5e7d22191440a04e28a075415dba1
Nikki Lionel Ocer <nlocer97@gmail.com>
2024-09-16 00:35:07 +0800

    Learn Static Web Server p2

--- server.js
+++ server.js
@@ -3,15 +3,10 @@ const path = require('path');
 
 const app = express();
 
+// setup static folder, render html pages
+// to use: http://localhost:8000/about.html
 app.use(express.static(path.join(__dirname, 'public')))
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
-
-// app.get("/about", (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'about.html'));
-// });
 
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
