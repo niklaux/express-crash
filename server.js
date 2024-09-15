10f77db609c819df15909a409c87088426676487
Nikki Lionel Ocer <nlocer97@gmail.com>
2024-09-16 00:29:54 +0800

    Learn res.sendFile() Method

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const path = require('path');
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("halooasdasdasd");
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get("/about", (req, res) => {
-  res.send("About page")
+  res.sendFile(path.join(__dirname, 'public', 'about.html'));
 });
 
 app.listen(8000, () => {
