4e4c6212d6f633f2706a9ad21474d1ed845f9820
Nikki Lionel Ocer <nlocer97@gmail.com>
2024-09-16 00:41:54 +0800

    working with json

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
 
 const app = express();
 
 // setup static folder, render html pages
 // to use: http://localhost:8000/about.html
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public')))
 
+let posts = [
+  { id: 1, title: "Titanic" },
+  { id: 2, title: "Fantasia" },
+];
+
+app.get("/api/posts", (req, res) => {
+  res.json(posts);
+});
 
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
